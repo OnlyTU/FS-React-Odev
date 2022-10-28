647b2269257582e856afb7539b52e57b9e7fada9
OnlyTU <tugberkmustafa@gmail.com>
2022-10-29 00:37:51 +0300

    React homeworks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const variants = [
+  {
+    color: "green",
+    name: "Green",
+    src: "green.jpg",
+  },
+  {
+    color: "yellow",
+    name: "Yellow",
+    src: "yellow.jpg",
+  },
+  {
+    color: "red",
+    name: "Red",
+    src: "red.jpg",
+  },
+];
 
 function App() {
+  const [selected, setSelected] = useState(variants[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {console.log("selected", selected)}
+      <div className="flex-container">
+        <div className="images">
+          <img src={`${selected.src}`} width="300"></img>
+        </div>
+        <div className="phone">
+          <div className="color-button">
+            <h2 className="phone-name">Apple Iphone 11</h2>
+            {variants.map((item, index) => (
+              <>
+                {console.log("item", item)}
+                <button
+                  className={item.color}
+                  style={{
+                    backgroundColor: selected == item ? item.color : "white",
+                  }}
+                  onClick={() => setSelected(variants[index])}
+                >
+                  {item.name}
+                </button>
+              </>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
